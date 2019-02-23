2c0bb3e1def717296ac18b828193f396f47a255e
Atwoa2 <47752525+Atwoa2@users.noreply.github.com>
2019-02-23 14:48:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Long
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Посчитаем длину окружности\nBведите радиус:");
+            string r = Console.ReadLine();
+            double x = Convert.ToDouble(r);
+            float Pi = 3.141592f;
+            Double d = Pi * x;
+            Console.WriteLine("Длина окружности:");
+            Console.WriteLine(d);
+
+            string result = d > 30 ? "Длина окружности больеш 30" : "Длина окружности меньше 30";
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
